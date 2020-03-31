43a9037a21a9e5b47b6ae8256b2601306b4bf67e
Yuta Obara <o.yuta722@gmail.com>
2020-03-30 23:02:40 -0400

    netlify form debug

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,7 +5,11 @@ const Contact = () => (
     <section id="contact" className="contact">
          <h2>CONTACT</h2>
          <p>Have any questions or wish to work together?</p>
-         <form className="contact__form" name="contact" method="POST" data-netlify="true">
+         <form className="contact__form" name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+                <input type="hidden" name="form-name" value="contact" />
+                <p class="contact__hidden">
+                    <label>Don’t insert: <input name="bot-field" /></label>
+                </p>
                 <label htmlFor="name" className="contact__label">NAME</label>
                 <input className="contact__input" type="text" name="name" />  
                 <label htmlFor="email" className="contact__label">EMAIL</label> 
